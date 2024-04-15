86220800a6e2ec019e308b89f372c876f5497238
Jeff Bothel <16275077+JeffBothel@users.noreply.github.com>
2024-04-14 23:32:50 -0600

    Addition of GUI elements to test case I added code to the test case that is supposed to be displaying account information and the various account stats as operations occur. This still needs some work done to it.
     Changes to be committed:
    	modified:   Module2/TestCase.java

--- Module2/TestCase.java
+++ Module2/TestCase.java
@@ -3,16 +3,58 @@ import javax.swing.*;
 
 public class TestCase extends JFrame {
     public static void main(String[] args) {
-        JFrame mainWindow = new JFrame("Panel");
-        JPanel mainPanel = new JPanel();
-        JLabel mainLabel = new JLabel("Panel Label");
-        JButton mainButton = new JButton("Panel Button");
+        // Create a new CheckingAccount object for testing
+        CheckingAccount testAccount = new CheckingAccount();
+        testAccount.setFirstName("John");
+        testAccount.setLastName("Doe");
+        testAccount.setInterestRate(0.07);
+        testAccount.setOverdraftFee(35.00);
+        System.out.println(testAccount.accountSummary());
 
-        mainPanel.add(mainLabel);
-        mainPanel.add(mainButton);
-
-        mainWindow.setSize(300, 200);
-        mainWindow.add(mainPanel);
+        // Creating the account information display
+        JFrame mainWindow = new JFrame("Account Information");
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setSize(400, 300);
+
+        // Setting up the action frame
+        JFrame actionFrame = new JFrame("Account Action");
+        actionFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        actionFrame.setSize(400, 300);
+        JPanel actionPanel = new JPanel();
+        actionPanel.setLayout(new BoxLayout(actionPanel, BoxLayout.Y_AXIS));
+        JLabel actionLabel = new JLabel("Enter the amount:");
+        actionPanel.add(actionLabel);
+        JTextField actionField = new JTextField();
+        actionField.setPreferredSize(new Dimension(100, 10));
+        actionPanel.add(actionField);
+        JButton actionWithdraw = new JButton("Withdraw");
+        actionWithdraw.addActionListener(e -> {
+            testAccount.processWithdrawal(Double.parseDouble(actionField.getText()));
+            actionFrame.setVisible(false);
+            mainWindow.setVisible(true);
+        });
+        actionPanel.add(actionWithdraw);
+        JButton actionDeposit = new JButton("Deposit");
+        actionPanel.add(actionDeposit);
+        actionPanel.add(actionWithdraw);
+        actionFrame.add(actionPanel);
+
+        // Setting up the main panel information
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        JTextPane accountInfo = new JTextPane();
+        accountInfo.setText(testAccount.accountSummary());
+        mainPanel.add(accountInfo);
+        JButton actionButton = new JButton("Account Action");
+        actionButton.addActionListener(e -> {
+            mainWindow.setVisible(false);
+            actionFrame.setVisible(true);
+        });
+        mainPanel.add(actionButton);
+
+        // Adding the panel to the frame
+        mainWindow.add(mainPanel);
+        mainWindow.setVisible(true);
+
     }
 }
